ee830640f1c542f341556150b65def7e65b86cc0
ItzCley <127433162+ItzCley@users.noreply.github.com>
2024-08-21 17:23:33 +0300

    Aura Hub.lua

--- Aura Hub.lua
+++ Aura Hub.lua
@@ -6,6 +6,7 @@ p.Size = Vector3.new(100, 10, 100)
 p.Transparency = 0
 p.Anchored = true  
 p.Parent = workspace
+local plr = game.Players.LocalPlayer
 local sp = 16
 local key = {}
 local monster = {}
@@ -13,7 +14,7 @@ local trickster ={}
 local locker = {}
 local OrionLib = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Orion/main/source'))()
 local Window = OrionLib:MakeWindow({
-	Name = "Aura Hub 🎗️",
+	Name = "| Aura Hub 🎗️ |",
 	HidePremium = false,
 	SaveConfig = true,
 	ConfigFolder = "AuraHub"
@@ -53,6 +54,25 @@ local arsenal = Window:MakeTab({
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
 })
+local tsb = Window:MakeTab({
+	Name = "The Strongest Battlegrounds",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+tsb:AddLabel("The Strongest Battlegrounds")
+tsb:AddButton({
+	Name = "FFj script",
+	Callback = function()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/FFJ1/Roblox-Exploits/main/scripts/TSBUtils.lua"))()
+	end    
+})
+tsb:AddButton({
+	Name = "Sillyware",
+	Callback = function()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/idontknowwhattonamemyself/Statue-Hub/Lua/Main"))()
+	end    
+})
+arsenal:AddLabel("Arsenal")
 arsenal:AddButton({
 	Name = "Shivam",
 	Callback = function()
@@ -71,6 +91,7 @@ arsenal:AddButton({
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/Insertl/QuotasHub/main/BETAv1.3"))()
 	end    
 })
+hood:AddLabel("Da hood")
 hood:AddButton({
 	Name = "AirHub",
 	Callback = function()
@@ -83,6 +104,7 @@ hood:AddButton({
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/spacexrandom/Lua/main/DaHood", true))()
 	end    
 })
+rival:AddLabel("Rivals")
 rival:AddButton({
 	Name = "Bailee",
 	Callback = function()
@@ -95,6 +117,7 @@ rival:AddButton({
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/KxGOATESQUE/SilentRivals/main/SilentRivals"))()
 	end    
 })
+ball:AddLabel("Blade Ball")
 ball:AddButton({
 	Name = "Visual beta",
 	Callback = function()
@@ -114,16 +137,24 @@ ball:AddButton({
         loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/b6cc646cf4d4eb44647236da9b7fa2f0.lua"))()
 	end    
 })
+Pet:AddLabel("Pet Simulator 99")
 Pet:AddButton({
 	Name = "Zap hub",
 	Callback = function()
 		loadstring(game:HttpGet('https://zaphub.xyz/Exec'))()
 	end    
 })
+Tab:AddLabel("Pressure")
 Tab:AddButton({
 	Name = "Pressure script",
 	Callback = function()
 		loadstring(game:HttpGet('https://raw.githubusercontent.com/Imgto/Aura-Hub/main/Pressure.txt'))()
+	end    
+})
+Tab:AddButton({
+	Name = "Invincibility (God Mode)",
+	Callback = function()
+		loadstring(game:HttpGet("https://rawscripts.net/raw/Pressure-*-Mode-15992"))()
 	end    
 })
 Pet:AddButton({
@@ -145,29 +176,12 @@ local credits = Window:MakeTab({
 })
 credits:AddParagraph("Credits to ItzCleyy", "Best developer")
 credits:AddParagraph("Also thanks to all developers thats helps me")
-others:AddSlider({
-	Name = "WalkSpeed🏃‍♂️",
-	Min = 16,
-	Max = 1000,
-	Default = 16,
-	Color = Color3.fromRGB(255,255,255),
-	Increment = 1,
-	ValueName = "WalkSpeed",
-	Callback = function(Value)
-    	game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = (Value)
-	end    
-})
-others:AddSlider({
-	Name = "JumpPower🦘",
-	Min = 50,
-	Max = 1000,
-	Default = 16,
-	Color = Color3.fromRGB(255,255,255),
-	Increment = 1,
-	ValueName = "JumpPower",
-	Callback = function(Value)
-        game.Players.LocalPlayer.Character.Humanoid.JumpPower = (Value)
-	end    
+others:AddLabel("Universal")
+others:AddButton({
+	Name = "Unc Test",
+	Callback = function()
+      		loadstring(game:HttpGet("https://raw.githubusercontent.com/unified-naming-convention/NamingStandard/main/UNCCheckEnv.lua"))()
+  	end    
 })
 others:AddButton({
 	Name = "Infinite yield",
